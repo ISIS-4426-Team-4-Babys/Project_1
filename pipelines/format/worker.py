f3588a3bc1f13627bc6b3328255affa8b39ec55e
JuanAriza17 <ja.arizag@uniandes.edu.co>
2025-09-02 13:56:23 -0500

    Factor Topic and Ollama

--- pipelines/format/worker.py
+++ pipelines/format/worker.py
@@ -1,8 +1,9 @@
 from subprocess import Popen, PIPE
 from rabbitmq import RabbitMQ
-import logging
 import requests
+import logging
 import time
+import json
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 OLLAMA_URL = "http://ollama:11434"
@@ -10,7 +11,7 @@ OLLAMA_URL = "http://ollama:11434"
 rabbitmq = RabbitMQ()
 
 logging.info("Verificando modelo llama3...")
-r = requests.post(f"{OLLAMA_URL}/api/pull", json={"model": "llama3"}, stream=True)
+r = requests.post(f"{OLLAMA_URL}/api/pull", json={"model": "deepseek-r1:latest"}, stream=True)
 
 # Leer la respuesta en streaming (Ollama devuelve progreso)
 for line in r.iter_lines():
@@ -29,20 +30,37 @@ def handle_request(ch, method, properties, body):
 
     prompt = "Convierte el contenido a un documento markdown estructurado, infiriendo los lugares" \
              "mas probables para poner títulos, subtítulos y otros elementos de formato. NO agregues " \
-             "información asicional a la dada por el contexto, simplemente formatea y devuelve un texto" \
+             "información adicional a la dada por el contexto, tampoco elimines información dada, simplemente formatea y devuelve un texto" \
              "que soporte una estrategia de segmentación por estructura de documento: " + markdown_text
     
     response = requests.post(f"{OLLAMA_URL}/api/generate", json={
-        "model": "llama3",
+        "model": "deepseek-r1:latest",
         "prompt": prompt,
         "stream": True
     })
+    
+    with open(markdown_path.replace(".md","_new.md"), "w", encoding="utf-8") as f:
+        for line in response.iter_lines(decode_unicode="utf-8"):
+            if line:
+                obj = json.loads(line)
+            try:
+                obj = json.loads(line)
+            except json.JSONDecodeError:
+                logging.debug(f"Línea no-JSON: {line!r}")
+                continue
 
-    logging.info("yikes")
+            chunk = obj.get("response")
+            if chunk:
+                logging.info("Procesando línea: %r", chunk)
+                f.write(chunk)
+                f.flush()
 
-    output = response.json().get("response", "")
+            # Cuando Ollama avisa que terminó, cortamos
+            if obj.get("done"):
+                break
 
-    logging.info(output)
+
+    logging.info("Generación completada.")
 
     rabbitmq.publish("vect", "Done")
 
