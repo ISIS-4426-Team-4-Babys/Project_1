05408520cebe2e98b5faa55adeb4ef220f85470f
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2025-09-04 10:45:04 -0500

    Deploy Async Worker Done

--- pipelines/deploy/templates/backend.py
+++ pipelines/deploy/templates/backend.py
@@ -1,1 +1,10 @@
-print("Esto ya funciona jejeje")
+import time
+
+print("Agente inicializado y en ejecución 🚀")
+
+try:
+    while True:
+        # Aquí en un futuro irán tus endpoints o lógica del agente
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Agente detenido")
